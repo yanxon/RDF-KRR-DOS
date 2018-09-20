a21bacee2384b619ba6782d5eda542c3d1de9e0b
Howard Yanxon <32254481+yanxon@users.noreply.github.com>
2018-09-19 23:05:35 -0700

    add some advance filtering of metallic materials

--- MP_RDF_DOS.py
+++ MP_RDF_DOS.py
@@ -3,11 +3,12 @@
 # From the structure, radial distribution function (RDF) can be constructed using the RDF code.
 # DOS at fermi level can also be extracted.
 
-
 # Importing libraries
 import numpy as np
 from pymatgen import MPRester
 from RDF import *
+from pymatgen.core.composition import Composition
+from pymatgen.core.periodic_table import Element
 
 # Input your MP ID
 m = MPRester('ZkyR13aTi9z5hLbX')
@@ -23,18 +24,56 @@ datas = m.query(criteria = {"band_gap": 0.0,
 formula = []
 ids = []
 
-points = len(ids)
+#for data in datas:
+#    formula.append(data['pretty_formula'])
+#    ids.append(data['material_id'])
+    
+#points = len(ids)
+
+sp_element = ["Be", "Li", "Na", "Mg", "Al", "K", "Ca", "Sc", "Ti", "V", "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn", "Ga","Rb", "Sr", "Y", "Zr", "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn"]
 
 for data in datas:
-    formula.append(data['pretty_formula'])
-    ids.append(data['material_id'])
+    compos = Composition(data['pretty_formula']).as_dict()
+    compos = list(compos.keys())
+    if len(compos) == 1:
+        if Element[compos[0]].symbol in sp_element:
+            formula.append(data['pretty_formula'] + ' ' + data['material_id'])
+            ids.append(data['material_id'])
+        else:
+            pass
+    elif len(compos) == 2:
+        if Element[compos[0]].symbol in sp_element:
+            if Element[compos[1]].symbol in sp_element:
+                formula.append(data['pretty_formula'] + ' ' + data['material_id'])
+                ids.append(data['material_id'])
+            else:
+                pass
+        else:
+            pass
+    elif len(compos) == 3:
+        if Element[compos[0]].symbol in sp_element:
+            if Element[compos[1]].symbol in sp_element:
+                if Element[compos[2]].symbol in sp_element:
+                    formula.append(data['pretty_formula'] + ' ' + data['material_id'])
+                    ids.append(data['material_id'])
+                else:
+                    pass
+            else:
+                pass
+        else:
+            pass
+
+    else:
+        pass
+
+
+points = len(ids)
 
 # Appending RDF and 
 X = [] # Reserve for RDF structure; X is 1D array.
 Y = [] # Reserve for DOS at fermi level; Y is 1D array.
-j = 0
 
-for i, identity in enumerate(ids):
+for i, identity in enumerate(ids[:2]):
     try:
         # Mining DOS
         dos = m.get_dos_by_material_id(identity)
@@ -42,23 +81,22 @@ for i, identity in enumerate(ids):
         total_dos = sum(dos.densities.values()) # Sum both spins, if present
         
         combine = np.vstack((energies, total_dos))
-        
+        print(combine)
         # Extracting the DOS at fermi level.
-        combine_abs = abs(combine[:,0])
+        combine_abs = abs(combine[0,:])
         find_ele_at_fermi = np.where(combine_abs == min(combine_abs))
         ele_at_fermi = find_ele_at_fermi[0][0]
 
         # Constructing RDF
         structure = m.get_structure_by_material_id(identity)
         struc = RDF(structure)
-        X.append(struc.RDF[:,1])
-        Y.append(combine[ele_at_fermi,1])
+        X.append(struc.RDF[1,:])
+        Y.append(combine[1,ele_at_fermi])
         		
-        print('Progress: ', i+1, '/', points)
+        print('Progress: ', i+1, '/', points, ' ---> ', formula[i])
         
     except:
-        del ids[i] #del the materials that don't have DOS in MP database
-        print('Progress: ', i+1, '/', points, ' is passed')
+        print('Progress: ', i+1, '/', points, ' is passed',  ' ---> ', formula[i])
         
-np.savetxt('C:/Resources/Install/plugins/data/X.txt', X)
-np.savetxt('C:/Resources/Install/plugins/data/Y.txt', Y)
+np.savetxt('X_.txt', X)
+np.savetxt('Y_.txt', Y)
