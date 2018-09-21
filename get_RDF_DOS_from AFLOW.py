3065e6a1984e995beee556636b1828c202d9e778
Howard Yanxon <32254481+yanxon@users.noreply.github.com>
2018-09-21 16:11:23 -0700

    Get RDF(X) to predict DOS(Y) from Aflow DB

--- get_RDF_DOS_from AFLOW.py
+++ get_RDF_DOS_from AFLOW.py
@@ -1,5 +1,8 @@
-# Explain code
+"""
+This code mines density of states (DOS) data from AFLOW database.
+AFLOW has DOS info stored in a .xz zipfile.
 
+"""
 
 # Import libraries
 from aflow import *
@@ -8,6 +11,49 @@ import numpy as np
 import pandas as pd
 from RDF import *
 
+def save_xz(filename, URL):
+    """
+    1. Save .xz zipfile downloaded from an online database.
+    2. Unzip the zipped files.
+    
+    Args:
+        URL: provide a URL of the database to look for the zipfile.
+
+        filename: provide the name of the file.
+                  filename should end with .xz
+    """
+    URL(filename)
+    zipfile = lzma.LZMAFile(filename).read()
+    newfilepath = filename[:-3]
+    fo = open(newfilepath+'.txt', 'wb').write(zipfile)
+    
+def get_DOS_fermi(filename):
+    """
+    This function takes DOS file and return the intensity at the fermi level.
+    
+    Args:
+        filename: provide the DOS file
+        
+    Return:
+        DOS at fermi level
+    """
+    with open(filename, 'r') as fin:
+        dataf = fin.read().splitlines(True)
+        fin.close()
+    with open(filename, 'w') as fout:
+        E_Fermi = [float(i) for i in dataf[5].split()][3]
+        fout.writelines(dataf[6:5006])
+        fout.close()
+        
+    DOS = np.genfromtxt(filename, dtype = float)
+    energy = DOS[:,0] - E_Fermi
+    dos = DOS[:,1]
+    combine = np.vstack((energy, dos))
+    combine_abs = abs(combine[0,:])
+    find_ele_at_fermi = np.where(combine_abs == min(combine_abs))
+    ele_at_fermi = find_ele_at_fermi[0][0]
+    
+    return combine[1,ele_at_fermi]
 
 # Get materials from AFLOW database based on the given criteria: 
 # metal and no more than 3 different elements.
@@ -17,42 +63,20 @@ results = search(batch_size = 100
 
 n = len(results) # number of avaiable data points
 
-
 X = [] # RDF of materials
 Y = [] # Density of states at fermi level
 
 for i, result in enumerate(results[2:5]):
-    # AFLOW has DOS info in a zip file (.xz).
-    # The following lines are to extract DOS from the zip file and
-    # to store the DOS info in your computer.
-    path = 'C:/Users/Carbon/Desktop/Aflow_DOS/' + result.compound + '.xz'
-    result.files['DOSCAR.static.xz'](path)
-    zipfile = lzma.LZMAFile(path).read()
-    newfilepath = path[:-4]
-    fo = open(newfilepath+'.txt', 'wb').write(zipfile)
-
-    with open(newfilepath+'.txt', 'r') as fin:
-        dataf = fin.read().splitlines(True)
-        fin.close()
-
-    with open(newfilepath+'.txt', 'w') as fout:
-        E_Fermi = [float(i) for i in dataf[5].split()][2]   #Extract Fermi energy
-        fout.writelines(dataf[6:5006])
-        fout.close()
+    URL = result.files['DOSCAR.static.xz']
+    save_xz(result.compound+'.xz', URL)
     
-    # Extract DOS at fermi level from the saved file.
-    DOS = np.genfromtxt(newfilepath+'.txt', dtype = float)
-    energy = DOS[:,0] - E_Fermi                             # E - Efermi
-    dos = DOS[:,1]
-    combine = np.vstack((energy, dos))
-    combine_abs = abs(combine[0,:])
-    find_ele_at_fermi = np.where(combine_abs == min(combine_abs))
-    ele_at_fermi = find_ele_at_fermi[0][0]
-    Y.append(combine[1,ele_at_fermi])
+    Y.append(get_DOS_fermi(result.compound+'.txt'))
 
     # Construct RDF from POSCAR which obtained from AFLOW
     crystal = Structure.from_str(result.files['CONTCAR.relax.vasp'](), fmt='poscar')
     X.append(RDF(crystal).RDF[1,:])
+    
+    print('progress: ', i, '/', n, ' materials is done')
 
 # Save RDF and DOS at fermi for Machine Learning
 np.savetxt('X_test.txt', X)
