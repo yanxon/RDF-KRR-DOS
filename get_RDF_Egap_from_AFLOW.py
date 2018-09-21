bb67201bdc76214863c5c0d72fda04e8573401f2
Howard Yanxon <32254481+yanxon@users.noreply.github.com>
2018-09-20 21:24:25 -0700

    Get RDF (X) to predict bandgap (Y) from Aflow DB
    Potentially, the obtained data will be feed into machine learning algorithm to be trained.

---
+++ get_RDF_Egap_from_AFLOW.py
@@ -0,0 +1,22 @@
+from RDF import *
+from aflow import *
+from math import pi
+from pymatgen.core.structure import Structure
+from pymatgen.symmetry.analyzer import SpacegroupAnalyzer
+
+results = search(batch_size=20
+        ).filter(K.Egap > 1
+        ).filter(K.nspecies == 2
+        ).filter(K.natoms < 5)
+print(len(results))
+
+X = []
+Y = []
+for result in results:
+    crystal = Structure.from_str(result.files['CONTCAR.relax.vasp'](), fmt='poscar')
+    X.append(RDF(crystal).RDF[:,1])
+    Y.append(result.Egap)
+
+#Store this file
+
+#Apply machine learning
