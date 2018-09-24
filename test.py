4d44a476589fce5afca91f97e401a80f00e4ba30
Howard Yanxon <32254481+yanxon@users.noreply.github.com>
2018-09-24 16:04:11 -0700

    test for new DOS

--- test.py
+++ test.py
@@ -2,36 +2,35 @@ import glob, os
 import numpy as np
 from pymatgen import MPRester
 
+m = MPRester('ZkyR13aTi9z5hLbX')
+datas = m.query(criteria = {#"band_gap": 0.0,
+                            'icsd_ids.0': {'$exists': True}
+                            },
+               properties=["pretty_formula", "material_id", "formula", "spacegroup.symbol", "icsd_ids"])
 
 os.chdir('prb_data')
 
 ICSD_IDs = []
-
 for file in glob.glob('*.dos'):
-    ICSD_IDs.append(int(file[:-4])
-    
-ICSD_IDs = np.reshape(ICSD_IDs, (3823,1))
+    ICSD_IDs.append(int(file[:-4]))
 
-print(ICSD_IDs[:10])
-
-
-m = MPRester('ZkyR13aTi9z5hLbX')
-datas = m.query(criteria = {"band_gap": 0.0,
-                            'icsd_ids.0': {'$exists': True}
-                            }, 
-               properties=["pretty_formula", "material_id", "formula", "spacegroup.symbol", "icsd_ids"])
+ICSD_IDs = np.reshape(ICSD_IDs, (3822,1))
 
 X = []
 Y = []
+mp_id = []
 
-for ids in ICSD_IDs[0:1000]:
-    for data in datas:
-        if ids in data["icsd_ids"]:
-            print('yes')
+#for data in datas:
+#    for id in ICSD_IDs:
+#        if id in data["icsd_ids"]:
+#            print(id, data["icsd_ids"])
+#            mp_id.append(data['material_id'])
 
-for data in datas[:10]:
-    print(data['icsd_ids'])
+# more efficient than the for loop above:
+for data in datas[0:10]:
+    for id in data['icsd_ids']:
+        if id in ICSD_IDs:
+            print(id, data['icsd_ids']
+            mp_id.append(data['material_id']
 
-datas[0]['icsd_ids'] in ICSD_IDs
-
-ICSD_IDs.append('25316')
+print(len(mp_id))
