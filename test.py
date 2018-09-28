d02ec4a270ecfc66deeb573a43d8f692514ea4ab
Howard Yanxon <32254481+yanxon@users.noreply.github.com>
2018-09-28 15:25:21 -0700

    just for testing

--- test.py
+++ test.py
@@ -9,8 +9,8 @@ datas = m.query(criteria = {#"band_gap": 0.0,
                             },
                properties=["pretty_formula", "material_id", "formula", "spacegroup.symbol", "volume", "icsd_ids"])
 
-X_ICSD_PRB = []         # RDF
-Y_ICSD_PRB = []         # DOS
+X = []         # RDF
+Y = []         # DOS
 sg_ICSD_PRB = []        # space group
 compound_ICSD_PRB = []  # compound
 mp_ids = []
@@ -23,30 +23,31 @@ for file in glob.glob('*.dos'):
         
 for ID in ICSD_IDs:
     # Look for each PRB ID in data screened in MP database
-    try:
-        for data in datas[0:20]:
-            if ID in data['icsd_ids']:
-                break
+#    try:
+    for data in datas:
+        if ID in data['icsd_ids']:
+            break
     
-        y = []
-        for line in open(str(ID)+'.dos','r'):
-            y.append(float(line))
-        y = y / data['volume']
+    y = []
+    for line in open(str(ID)+'.dos','r'):
+        y.append(float(line))
+    y = np.asarray(y) / data['volume']
         
-        structure = m.get_structure_by_material_id(data['material_id'])
-        struc = RDF(structure)
-        X_ICSD_PRB.append(struc.RDF[1,:])
-        Y_ICSD_PRB.append(y)
-        mp_ids.append(data['material_id'])
-        sg_ICSD_PRB.append(data['spacegroup.symbol'])
-        compound_ICSD_PRB.append(data['pretty_formula'])
+    structure = m.get_structure_by_material_id(data['material_id'])
+    struc = RDF(structure)
+    X.append(struc.RDF[1,:])
+    Y.append(y)
+#        mp_ids.append(data['material_id'])
+#        sg_ICSD_PRB.append(data['spacegroup.symbol'])
+#        compound_ICSD_PRB.append(data['pretty_formula'])
         
-        print(data['material_id'], ' is stored')
-    except:
-        pass
+    print(data['material_id'], ' is stored')
+        
+#    except:
+#        pass
 
-np.savetxt('X_ICSD_PRB.txt', X_ICSD_PRB)
-np.savetxt('Y_ICSD_PRB.txt', Y_ICSD_PRB)
+np.savetxt('X_ICSD_PRB.txt', X)
+np.savetxt('Y_ICSD_PRB.txt', Y)
 np.savetxt('sg_ICSD_PRB.txt', sg_ICSD_PRB)
 np.savetxt('compound_ICSD_PRB.txt', compound_ICSD_PRB)
 np.savetxt('mp_id.txt', mp_ids)
