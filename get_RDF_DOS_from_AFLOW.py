92e63e29b132a3e814c8cfbf16dc3498be26f6ac
Howard Yanxon <32254481+yanxon@users.noreply.github.com>
2018-10-02 22:42:19 -0700

    get RDF & DOS from AFLOW train with KRR

--- get_RDF_DOS_from_AFLOW.py
+++ get_RDF_DOS_from_AFLOW.py
@@ -1,11 +1,14 @@
 """
 This code mines density of states (DOS) data from AFLOW database.
 AFLOW has DOS info stored in a .xz zipfile.
-Here is the overall idea:
+The overall idea:
+a. Mining:
     1. Mine data from AFLOW in .xz zipfile
     2. Convert the .xz zipfile into .txt
     3. read DOS information from .txt
     4. append as Y (array) and save it as a textfile.
+b. Machine Learning:
+    Training with KRR
 """
 
 # Import libraries
@@ -17,7 +20,13 @@ from RDF import *
 import os
 from pymatgen.core.composition import Composition
 from pymatgen.core.periodic_table import Element
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.kernel_ridge import KernelRidge
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import mean_absolute_error
 
+# Functions
 def save_xz(filename, URL):
     """
     1. Save .xz zipfile downloaded from an online database.
@@ -108,35 +117,24 @@ def get_d_metal():
     metals.append('Zr')
     return metals
 
-
-sp_system = get_s_metal() + get_p_metal()
-spd_system = get_d_metal()
-
+####################################### Part a: Mining ###########################################
 # Get materials from AFLOW database based on the given criteria: 
 # metal and no more than 3 different elements.
+
+sp_system = get_s_metal() + get_p_metal()
+
 results = search(batch_size = 100
                 ).filter(K.Egap_type == 'metal'
                 ).filter(K.nspecies < 7)
 
 n = len(results) # number of avaiable data points
 
-#X_all_metals = [] # RDF of materials
-#Y_all_metals = [] # Density of states at fermi level
-#sg_all_metals = [] # Space group of all metal
-#compounds_all_metals = [] #compounds of all metal
-
 X_sp_metals = []
 Y_sp_metals = []
 sg_sp_metals = []
 compounds_sp_metals = []
 
-
-#X_spd_metals = []
-#Y_spd_metals = []
-#sg_spd_metals = []
-#compounds_spd_metals = []
-
-for i, result in enumerate(results[:5]):
+for i, result in enumerate(results):
     try:
         if result.catalog == 'ICSD\n':
             URL = result.files['DOSCAR.static.xz']
@@ -144,12 +142,6 @@ for i, result in enumerate(results[:5]):
     
             # Construct RDF with POSCAR
             crystal = Structure.from_str(result.files['CONTCAR.relax.vasp'](), fmt='poscar')
-            
-            # Appending for all metals
-#            X_all_metals.append(RDF(crystal).RDF[1,:])
-#            Y_all_metals.append(get_DOS_fermi(result.compound+'.txt', result))
-#            sg_all_metals.append(result.spacegroup_relax)
-#            compounds_all_metals.append(result.compound)
             
             # Get elements in the compound
             elements = result.species
@@ -168,36 +160,103 @@ for i, result in enumerate(results[:5]):
                 sg_sp_metals.append(result.spacegroup_relax)
                 compounds_sp_metals.append(result.compound)
                 
-#            # Appending for spd_metals
-#            j = 0
-#            for element in elements:
-#                if element in spd_system:
-#                    j += 1
-#            if j == len(elements):
-#                X_spd_metals.append(RDF(crystal).RDF[1,:])
-#                Y_spd_metals.append(get_DOS_fermi(result.compound+'.txt', result))
-#                sg_spd_metals.append(result.spacegroup_relax)
-#                compounds_spd_metals.append(result.compound)
-            
-            print('progress: ', i+1, '/', n, ' materials is done')
+            print('progress: ', i+1, '/', n, '-------- material is stored')
             
     except:
+        print('progress: ', i+1, '/', n, '-------- material does not fit the criteria')
         pass
 
-# Save as a text file for all metals
-#np.savetxt('X_all_metals.txt', X_all_metals)
-#np.savetxt('Y_all_metals.txt', Y_all_metals)
-#np.savetxt('sg_all_metals.txt', sg_all_metals)
-#np.savetxt('compounds_all_metals.txt', compounds_all_metals, delimiter=" ", fmt="%s")
+################################ Part b: Machine Learning ###################################
 
-# Save as a text file for sp metals
-np.savetxt('X_sp_metals.txt', X_sp_metals)
-np.savetxt('Y_sp_metals.txt', Y_sp_metals)
-np.savetxt('sg_sp_metals.txt', sg_sp_metals)
-np.savetxt('compounds_sp_metals.txt', compounds_sp_metals, delimiter=" ", fmt="%s")
+N_data = len(sg_sp_metals)
 
-## Save as a text file for spd metals
-#np.savetxt('X_spd_metals.txt', X_spd_metals)
-#np.savetxt('Y_spd_metals.txt', Y_spd_metals)
-#np.savetxt('sg_spd_metals.txt', sg_spd_metals)
-#np.savetxt('compounds_spd_metals.txt', compounds_spd_metals, delimiter=" ", fmt="%s")
+# Shorter RDF
+X_sp_metals = X_sp_metals[:,10:30]
+
+# Running a GridSearch to determine the best parameters
+X_train, X_test, Y_train, Y_test = train_test_split(X_sp_metals, Y_sp_metals, test_size = 0.1, random_state=0)
+estimator = GridSearchCV(KernelRidge(kernel='laplacian', gamma=0.1), cv=10, 
+                         param_grid={"alpha": [1e6, 1e5, 1e4, 1e3, 100, 10, 1e0, 0.1, 1e-2, 1e-3, 1e-4, 1e-5, 1e-6], 
+                         "gamma": np.logspace(-5, 5)})
+estimator.fit(X_train, Y_train)
+
+best_alpha = estimator.best_params_['alpha']
+best_gamma = estimator.best_params_['gamma']
+
+# Train with the best parameters
+estimator2 = KernelRidge(alpha = best_alpha, coef0 = 1, gamma = best_gamma, kernel='laplacian', kernel_params=None)
+estimator2.fit(X_train, Y_train)
+y_predicted = estimator2.predict(X_test)
+r2= estimator2.score(X_test, Y_test, sample_weight=None)
+print('r^2 = ', r2)
+
+mae = mean_absolute_error(y_predicted, Y_test)
+print(mae)
+
+# Plotting
+n_test = len(Y_test)
+
+Y_testing=[]
+sg_testing=[]
+for i in range(n_test):
+    for j in range(N_data):
+        if Y_test[i,0] == Y_sp[j,0]:
+            sg_testing.append(sg_sp[j])
+            break
+
+Y_test_triclinic = []
+y_pred_triclinic = []
+Y_test_monoclinic = []
+y_pred_monoclinic = []
+Y_test_orthorhombic = []
+y_pred_orthorhombic = []
+Y_test_tetragonal = []
+y_pred_tetragonal = []
+Y_test_trigonal = []
+y_pred_trigonal = []
+Y_test_hexagonal = []
+y_pred_hexagonal = []
+Y_test_cubic = []
+y_pred_cubic = []
+
+for i, sg in enumerate(sg_testing):
+    if sg in [1,2]:
+        Y_test_triclinic.append(Y_test[i,:])
+        y_pred_triclinic.append(y_predicted[i,:])
+    elif sg in np.arange(3,16):
+        Y_test_monoclinic.append(Y_test[i,:])
+        y_pred_monoclinic.append(y_predicted[i,:])
+    elif sg in np.arange(16,75):
+        Y_test_orthorhombic.append(Y_test[i,:])
+        y_pred_orthorhombic.append(y_predicted[i,:])
+    elif sg in np.arange(75,143):
+        Y_test_tetragonal.append(Y_test[i,:])
+        y_pred_tetragonal.append(y_predicted[i,:])
+    elif sg in np.arange(143,168):
+        Y_test_trigonal.append(Y_test[i,:])
+        y_pred_trigonal.append(y_predicted[i,:])
+    elif sg in np.arange(168,195):
+        Y_test_hexagonal.append(Y_test[i,:])
+        y_pred_hexagonal.append(y_predicted[i,:])
+    elif sg in np.arange(195,231):
+        Y_test_cubic.append(Y_test[i,:])
+        y_pred_cubic.append(y_predicted[i,:])
+        
+tri = plt.scatter(y_pred_triclinic, Y_test_triclinic, c='green')
+mono = plt.scatter(y_pred_monoclinic, Y_test_monoclinic, c='red')
+ortho = plt.scatter(y_pred_orthorhombic, Y_test_orthorhombic, c='black')
+tetra = plt.scatter(y_pred_tetragonal, Y_test_tetragonal, c='gold')
+trig = plt.scatter(y_pred_trigonal, Y_test_trigonal, c='purple')
+hexa = plt.scatter(y_pred_hexagonal, Y_test_hexagonal, c='darkcyan')
+cub = plt.scatter(y_pred_cubic, Y_test_cubic, c='gray')
+
+plt.title('DOS: Actual vs Predicted -- 844 crystals')
+#plt.text(1.6,.3, '$r^2$: 0.6540')
+plt.legend((tri, mono, ortho, tetra, trig, hexa, cub),
+           ('triclinic', 'monoclinic', 'orthorhombic', 'tetragonal', 'trigonal', 'hexagonal', 'cubic'),
+           loc='lower right',
+           fontsize = 7)
+plt.xlabel('y_predicted')
+plt.ylabel('Y_test')
+plt.savefig('KRR_sp_metals.png')
+plt.show()
